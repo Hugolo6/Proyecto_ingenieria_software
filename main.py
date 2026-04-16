1d076bd164a0a442fa8ebc5c54be8d3892f23a71
HugoMIW <l23590549@sjuanrio.tecnm.mx>
2026-04-16 12:01:54 -0600

    primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Ingenieria de software
+# PUNTO DE VENTA
+# Equipo:
+# Garcia Hernandez Andres Jesus
+# Martinez Briseño Hugo Francisco 
+# Bertadillo Armenta Angel de Jesus
+
+class Partida:
+
+    def __init__(self):
+        self.usuario = "El guayaba"
+        self.precio = ""
+        self.producto
